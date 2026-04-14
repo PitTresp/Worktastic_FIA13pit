1b85bf8b1f486fb1b701ff349e102b55af8fcdb7
nadja.reuther <nadja_reuther@freenet.de>
2026-04-14 10:10:32 +0200

    Projektdateien hinzufügen.

---
+++ Worktastic/Controllers/JobPostingController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Worktastic.Controllers
+{
+    public class JobPostingController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult CreateEditForm(int id)
+        {
+            //TODO
+            return View();
+        }
+    }
+}
---
+++ Worktastic/Models/JobPosting.cs
@@ -0,0 +1,18 @@
+﻿namespace Worktastic.Models
+{
+    public class JobPosting
+    {
+        public int Id { get; set; }
+        public string JobTitle { get; set; }
+        public string? JobDescription { get; set; }  
+        public string JobLocation { get; set; }
+        public DateTime StartDate { get; set; }
+        public int? Salary { get; set; }
+        public string ContactName { get; set; }
+        public string ContactEmail { get; set; }
+        public string? ContactPhone { get; set; }
+        public string? ContactWebsite { get; set; }
+        public byte[]? CompanyLogo { get; set; }
+        public string? OwnerName { get; set; }
+    }
+}
