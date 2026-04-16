69fdefae0c8c32278cc28c83c75cda2def0b2817
nadja.reuther <nadja_reuther@freenet.de>
2026-04-16 12:06:21 +0200

    ApiController integrated GetAll GetById Create (begin)

---
+++ Worktastic/Controllers/ApiJobPostingController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using SQLitePCL;
+using Worktastic.Data;
+using Worktastic.Models;
+
+namespace Worktastic.Controllers
+{
+    [Route("api/jobposting")]
+    [ApiController]
+
+    public class ApiJobPostingController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+        public ApiJobPostingController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet("GetAll")]
+        public IActionResult GetAll()
+        {
+            var jobsFromDb = _context.JobPosts.ToArray();
+            return Ok(jobsFromDb);
+        }
+
+        [HttpGet("GetById")]
+        public IActionResult GetById(int id)
+        {
+            var jobFromDb = _context.JobPosts.SingleOrDefault(x => x.Id == id);
+            if (jobFromDb == null) return NotFound();
+            return Ok(jobFromDb);
+        }
+
+        [HttpPost("Create")]
+        public IActionResult Create(JobPosting job)
+        {
+            //Todo eintragen in Database
+        }
+    }
+}
